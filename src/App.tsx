ad2f28d4878429d1394fa4afe7dbfe5c4466356d
koladev <onaelmangabo@gmail.com>
2022-01-26 13:35:37 +0100

    - feat: add redux counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import { useSelector, useDispatch } from "react-redux";
+import {increment} from "./store/actions"
 
 
 function App() {
+
+  const {counter} = useSelector((state:any) => state);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      Hi
+      <h1>
+         Hello World <br /> A simple counter made with Redux!
+      </h1>
+      <h3>Counter</h3>
+      <h3>{counter}</h3>
+      <button onClick={() => dispatch(increment())}>Increase</button>
     </div>
   );
 }
