bee82eeb7bede4efaf005d757ccb03096159d0c8
koladev <onaelmangabo@gmail.com>
2022-01-26 13:47:16 +0100

    - feat: add recoil to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { atom, useRecoilState } from 'recoil';
+
+const counterState = atom({
+  key: "counterState",
+  default: 1,
+})
 
 function App() {
+
+  const [counter, setCounter] = useRecoilState(counterState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+         Hello World <br /> A simple counter made with Redux!
+      </h1>
+      <h3>Counter</h3>
+      <h3>{counter}</h3>
+      <button onClick={() => setCounter(counter + 1)}>Increase</button>
     </div>
   );
 }
